100517685e25b3e96dcd1d5c21d1b7e17913a6d4
Nitin Pathak <nitinpathak8242@gmail.com>
2025-01-21 21:02:16 +0530

    redirect to login if not authenticated

---
+++ src/components/Body.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect } from 'react'
+import NavBar from './NavBar'
+import Profile from './Profile'
+import Login from './Login'
+import { Outlet, useNavigate } from 'react-router-dom'
+import Footer from './Footer'
+import { BASE_URL } from '../utils/constants'
+import { useDispatch, useSelector } from 'react-redux'
+import { addUser } from '../utils/userSlice'
+import axios from "axios";
+
+const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+
+  const fetchUser = async () => {
+    if(userData) return;
+    try{
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data));
+    }
+    catch(err){
+      if(err.status == 401){
+        navigate("/login");
+      }
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser(); 
+  }, []);
+
+  return (
+    <div>
+      <NavBar/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Body
+
+
+
+
---
+++ src/components/Feed.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Feed = () => {
+  return (
+    <div>
+      <h1>Feed Component</h1>
+    </div>
+  )
+}
+
+export default Feed
+
+
+

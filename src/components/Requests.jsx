87964e09d3e9858275e9f25a27ed8e24c0b17ee6
Nitin Pathak <nitinpathak8242@gmail.com>
2025-01-24 21:01:58 +0530

    completed interested and ignore field

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -1,12 +1,25 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BASE_URL } from '../utils/constants';
 import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
-import { addRequests } from '../utils/requestSlice';
+import { addRequests, removeRequest } from '../utils/requestSlice';
 
 const Requests = () => {
     const requests = useSelector((store) => store.requests);
     const dispatch = useDispatch();
+
+    const [showButtons, setShowButtons] = useState(true);
+
+    const reviewRequest = async (status, _id) => {
+        try{
+            const res = axios.post(BASE_URL + "/request/review/" + status + "/" + _id, {}, {withCredentials: true});
+
+            dispatch(removeRequest(_id));
+        }
+        catch(err){
+            console.error(err);
+        }
+    };
 
     const fetchRequests = async () => {
         try{
@@ -23,7 +36,7 @@ const Requests = () => {
     }, []);
 
     if(!requests) return;
-    if(requests.length == 0) return <h1> No Requests Found </h1>
+    if(requests.length == 0) return <h1 className="flex justify-center my-10"> No Requests Found </h1>
 
     return (
         <div className="text-center my-10">
@@ -40,8 +53,8 @@ const Requests = () => {
                             <p>{about}</p>
                         </div> 
                         <div>
-                            <button className="btn btn-primary mx-2">Reject</button>
-                            <button className="btn btn-secondary mx-2">Accept</button>
+                            <button className="btn btn-primary mx-2" onClick={() => reviewRequest("Rejected", request._id)}>Reject</button>
+                            <button className="btn btn-secondary mx-2" onClick={() => reviewRequest("Accepted", request._id)}>Accept</button>
                         </div> 
                     </div>
                 );
